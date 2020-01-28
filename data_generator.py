63e2a8e7a2352a8f412309df2c174fa9ad730332
Nicola <nicola.dainese96@gmail.com>
2020-01-28 16:12:33 +0100

    Added function to get MNIST dataset in numpy

--- data_generator.py
+++ data_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from torchvision.datasets import MNIST
 
 def create_dataset(n_samples, linear_dim=5, sigma=0.5, prob_zero=0.5):
     """
@@ -15,8 +16,8 @@ def create_dataset(n_samples, linear_dim=5, sigma=0.5, prob_zero=0.5):
     prob_zero : float in [0,1], 
         probability that an image is created according to pattern 0
         
-    Returns
-    -------
+    Return
+    ------
     data : numpy array of floats in [0,1] of shape (n_samples, linear_dim, linear_dim)
         array of greyscale images
     labels : numpy array of int of length n_samples
@@ -47,3 +48,37 @@ def create_dataset(n_samples, linear_dim=5, sigma=0.5, prob_zero=0.5):
     data = data*(1-sigma)+noise
     
     return data, labels
+
+def get_MNIST_dataset(data_root_dir = './datasets', download=True):
+    """
+    Import MNIST dataset in numpy splitted in training and test sets.
+    
+    Parameters
+    ----------
+    data_root_dir : str,
+        path of the directory where to download or import the dataset
+    download: bool,
+        downloads the dataset from http://yann.lecun.com/exdb/mnist/
+        
+    Return
+    ------
+    train_data : numpy array, float, shape (60000, 28, 28)
+    train_labels : numpy array, int, shape (60000,)
+    test_data : numpy array, float, shape (10000, 28, 28)
+    test_labels : numpy array, int, shape (10000,)
+    
+    Notes
+    -----
+    Requires torchvision library
+
+    """
+    torch_train_dataset = MNIST(data_root_dir, train=True,  download=download)
+    torch_test_dataset  = MNIST(data_root_dir, train=False, download=download)
+    
+    train_data = np.array([np.array(x[0]) for x in torch_train_dataset])
+    train_labels = np.array([np.array(x[1]) for x in torch_train_dataset])
+
+    test_data = np.array([np.array(x[0]) for x in torch_test_dataset])
+    test_labels = np.array([np.array(x[1]) for x in torch_test_dataset])
+    
+    return train_data, train_labels, test_data, test_labels
